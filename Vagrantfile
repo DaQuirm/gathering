1d97ebae07b6946a05a52cb7c57c533a18c58bf6
ttrffun <ttrffun@gmail.com>
2014-03-29 21:28:58 +0300

    Added git to Vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,9 @@ Vagrant::Config.run do |config|
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
 
   config.vm.share_folder "v-root", "/gathering", "."
-  config.vm.forward_port 3000, 8081
+  config.vm.forward_port 3000, 8081, auto_correct: false
 
   config.vm.provision "shell", path: "provision.sh"
+
+  config.vm.host_name = "gathering"
 end
--- provision.sh
+++ provision.sh
@@ -4,9 +4,12 @@ if [ -f "/var/vagrant_provision" ]; then
 	exit 0
 fi
 
+export NODE_VERSION="0.10.26"
+export GIT_VERSION="1.9.1"
+
 touch /var/vagrant_provision
 
-
+# Provisioning
 echo "[Vagrant provisioning]"
 
 echo ".. Updating & upgrading index"
@@ -18,15 +21,34 @@ echo ".. Installing g++"
 apt-get install -y g++ >/dev/null
 
 echo ".. Downloading node"
-wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz >/dev/null 2>&1
+wget "http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz" >/dev/null 2>&1
 echo ".. Extracting node"
-tar -xzf node-v0.10.26.tar.gz >/dev/null
-cd ./node-v0.10.26 >/dev/null
+tar -xzf "node-v$NODE_VERSION.tar.gz" >/dev/null
+cd "./node-v$NODE_VERSION" >/dev/null
 echo ".. Making node"
 ./configure >/dev/null
 make >/dev/null
 make install >/dev/null
 cd ../
 echo ".. Cleanup after making node"
-rm -rf node-v0.10.26.tar.gz >/dev/null
+rm "node-v$NODE_VERSION.tar.gz" >/dev/null
+rm -rf "node-v$NODE_VERSION" >/dev/null
 echo "The node version installed: " & node -v
+
+echo ".. Installing git"
+echo ".. Downloading git dependencies"
+apt-get install -y curl expat gettext openssl zlib1g-dev >/dev/null
+echo ".. Downloading git"
+wget "https://www.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz" >/dev/null
+echo ".. Extracting git"
+tar -xzf "git-$GIT_VERSION.tar.gz" >/dev/null
+cd "./git-$GIT_VERSION" >/dev/null
+echo ".. Making git"
+./configure >/dev/null
+make prefix=/usr all >/dev/null
+make prefix=/usr install >/dev/null
+cd ../
+echo ".. Cleanup after making git"
+rm "git-$GIT_VERSION.tar.gz" >/dev/null
+rm -rf "git-$GIT_VERSION" >/dev/null
+echo "The git version installed: " & git --version
